9200d8e79144a0f8ed1e9db6ab0a789ba8080e66
teghfo <teghfo@gmail.com>
2022-02-24 12:07:10 +0330

    jalase charom

---
+++ Basic/J4.py
@@ -0,0 +1,7 @@
+from calculator import *
+from J3 import *
+
+# matrix_mulyiply()
+
+print("in jalaseh 4: ", __name__)
+print("in jalaseh 4")
---
+++ Basic/calculator/logarithm.py
@@ -0,0 +1,8 @@
+print("logarithm in module")
+
+def _logarithm():
+    return "logarithm"
+
+
+def test_a():
+    print("test a in package")
