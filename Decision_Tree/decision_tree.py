0cd354440ecf7fff5a6076f99ba8a1b6a92f1c64
teghfo <teghfo@gmail.com>
2022-06-02 14:17:51 +0430

    week9: DT

---
+++ Decision_Tree/decision_tree.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+import graphviz
+
+iris_data = load_iris()
+
+iris_df = pd.DataFrame(iris_data.data, columns=iris_data.feature_names)
+iris_df["target"] = iris_data.target
+
+print(iris_data.feature_names)
+X = iris_data.data
+y = iris_data.target
+
+X_train , X_test, y_train, y_test = train_test_split(X, y, stratify=iris_df.target, test_size=0.3, random_state=42)
+
+
+dt_classifier = DecisionTreeClassifier()
+
+dt_classifier.fit(X_train, y_train)
+# tree.plot_tree(dt_classifier)
+
+dot_data = tree.export_graphviz(dt_classifier, out_file=None)
+graph = graphviz.Source(dot_data)
+graph.render("iris")
+
+print(classification_report(y_test, dt_classifier.predict(X_test)))
